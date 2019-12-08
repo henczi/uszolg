c8a69901372226dc9635b4a020e346d3660be8e8
Henczidai Bálint <henczidaibalint@gmail.com>
2019-12-08 18:39:07 +0100

    feedback

---
+++ src/feedback/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return jsonify(status="OK"), 200
+
+@app.route('/send', methods=['POST'])
+def send():
+    fb = request.get_json()
+    print(fb)
+    # send mail, store db, pass to analitics, etc ..
+    return jsonify(status="OK", message="Your feedback is important to us!", data=fb), 200
+
+if __name__ == '__main__':
+    app.run()
