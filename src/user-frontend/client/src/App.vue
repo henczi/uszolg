6490d433e1e3131ed5aeb2e8a9459c69c376b713
Henczidai Bálint <henczidaibalint@gmail.com>
2019-12-09 10:07:31 +0100

    dev dockerfil

--- src/user-frontend/client/src/App.vue
+++ src/user-frontend/client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome 23 to Your Vue.js App"/>
+    <HelloWorld msg="Welcome 2::3 to Your Vue.js App"/>
   </div>
 </template>
 
