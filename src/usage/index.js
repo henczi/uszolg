cd157f46675df71713bee8293d464f281e92b8f0
Henczidai Bálint <henczidaibalint@gmail.com>
2019-12-12 14:20:27 +0100

    rabbitmq

--- src/usage/index.js
+++ src/usage/index.js
@@ -1,6 +1,38 @@
 var express = require('express');
+var amqp = require('amqplib');
 var bodyParser = require('body-parser');
 var app = express();
+
+var queue = 'usage';
+const DB = {};
+
+async function wait(n) { return new Promise(r => setTimeout(r, n)); }
+
+async function retryConnec(max) {
+	console.log('[RabbitMQ] Retry connect (' + max + ') ... ');
+	try {
+		return await amqp.connect('amqp://rabbitmq');
+	} catch (e) {
+		if (max == 0) throw (e);
+		await wait(5000);
+		return await retryConnec(max-1);
+	}
+}
+
+(async function () {
+	var conn = await retryConnec(10);
+	console.log('[RabbitMQ] Connected!');
+	var channel = await conn.createChannel();
+	var queueInfo = channel.assertQueue(queue, { durable: true });
+	channel.consume(queue, function(message) {
+		if (message !== null) {
+			console.log('[QUE::msg] ' + message.content.toString());
+			console.log(message);
+			channel.ack(message);
+		}
+	})
+})();
+
 
 /**
  * Parse parameters in POST
