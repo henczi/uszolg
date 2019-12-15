9560f238fda093d7735839d2077035cb5145295a
Henczidai Bálint <henczidaibalint@gmail.com>
2019-12-15 22:20:11 +0100

    fwauth

---
+++ src/user-auth/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UserAuth.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class AuthController : ControllerBase
+    {
+        private static ConcurrentDictionary<string, bool> bans = new ConcurrentDictionary<string, bool>();
+
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (Request.Headers.TryGetValue("X-User-Id", out var userId) && !bans.ContainsKey(userId))
+            {
+                Console.WriteLine("Auth - 200 " + userId);
+                return StatusCode(200);
+            }
+            Console.WriteLine("Auth - 401 " + userId);
+            return StatusCode(401);
+        }
+
+        [HttpGet("ban/{id}")]
+        public string Ban(string id)
+        {
+            var added = bans.TryAdd(id, true);
+            Console.WriteLine($"Ban - {added} - {id}");
+            return added ? "Added" : "Error";
+        }
+
+        [HttpGet("test")]
+        public string Test(string id)
+        {
+            return "OK";
+        }
+    }
+}
--- src/user-frontend/client/src/main.js
+++ src/user-frontend/client/src/main.js
@@ -3,11 +3,28 @@ import App from './App.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 import BootstrapVue from 'bootstrap-vue'
 
+import axios from 'axios';
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import "@fortawesome/fontawesome-free/css/all.css";
 
+
+function uuidv4() {
+  if (!window.crypto || !window.crypto.getRandomValues)
+    return (+new Date) + "_" + ~~(1000000000 * Math.random());
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+let userId = localStorage.getItem('userId');
+if (userId == null) {
+  userId = uuidv4();
+  localStorage.setItem('userId', userId);
+}
+axios.defaults.headers.common['X-User-Id'] = userId;
 
 Vue.config.productionTip = false
 
